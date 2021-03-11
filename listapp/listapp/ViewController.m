e388a55f61751081bbe45de7b25cdfcc0284756b
Jack Curtin <jcurtin4@binghamton.edu>
2021-03-10 22:50:24 -0500

    Finish video demo, finally displaying screen

--- listapp/listapp/ViewController.m
+++ listapp/listapp/ViewController.m
@@ -12,11 +12,34 @@
 @end
 
 @implementation ViewController
+@synthesize tableView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [tableView setDataSource:self];
+    [tableView setDelegate:self];
+    
     // Do any additional setup after loading the view.
 }
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 100;
+}
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if(!cell){
+        cell = [[UITableViewCell alloc] init];
+    }
+    [[cell textLabel] setText: [NSString stringWithFormat:@"Cell %d", (int) [indexPath row]]];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"Select row %d", (int) [indexPath row]);
+}
 
 @end
